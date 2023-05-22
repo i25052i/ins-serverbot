95fa01aa7a835a91a9c2741b71f2676a1b3b5bbf
Hansen <hdan2580@gmail.com>
2023-05-21 20:36:11 -0700

    watcher reads stdin

--- logwatcher/main.cpp
+++ logwatcher/main.cpp
@@ -5,5 +5,24 @@
 
 int main(int argc, char** argv[]) {
 
+    /* Startup Messages */
+    std::cout << "ins-serverbot" << std::endl;
+    std::cout << "Version " << logwatcher_VERSION_MAJOR << "." 
+              << logwatcher_VERSION_MINOR << std::endl;
+    std::cout << "Starting..." << std::endl;
+
+    /* Consume server logs */
+    while (true) {
+        std::string line;
+
+        std::getline(std::cin, line);
+
+        if (line.empty()) {
+            continue;
+        }
+
+        std::cout << line << std::endl;
+    }
+
     return 0;
 }
